d8875ea3801ec85826376e4e75bcd8676c8776b2
carlesnacher <carlesnacher@gmail.com>
2022-11-22 11:56:05 +0100

    Add metodos dado.comoponent.ts

--- src/app/elemento/dado/dado.component.ts
+++ src/app/elemento/dado/dado.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dado',
   templateUrl: './dado.component.html',
   styleUrls: ['./dado.component.css']
 })
-export class DadoComponent {
+export class DadoComponent implements OnInit{
+
+  @Input() minimo: any;
+  @Input() maximo: any;
+  actual: any;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.actual = this.minimo;
+  }
+
+  incrementar() {
+    if (this.actual<this.maximo)
+      this.actual++;
+  }
+
+  decrementar() {
+    if (this.actual>this.minimo)
+      this.actual--;
+  }
+
+  fijar(v:number) {
+    if (v>=this.minimo && v<=this.maximo)
+      this.actual=v;
+  }
 
 }
